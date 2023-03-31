9368d0b77259e314d77f013e5d1142381de60650
Thomas <mail@thomasfleischer.com>
2023-03-31 14:50:57 +0200

    PHP8 compatibility

--- src/Builder.php
+++ src/Builder.php
@@ -50,7 +50,9 @@ class Builder extends EloquentBuilder
      */
     public function callParent($method, array $args)
     {
-        return call_user_func_array("parent::{$method}", $args);
+        // PHP8 compatibility
+        return parent::$method(...array_values($args));
+        //return call_user_func_array("parent::{$method}", $args);
     }
 
     /**
